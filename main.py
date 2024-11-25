fdcb506f81126d3b2ce64431678db0dd51063fbc
Christopher Akins <christopher.v.akins@gmail.com>
2024-11-24 19:57:11 -0700

    movement of player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   clock = pygame.time.Clock()
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   dt = 0
 
   while True:
@@ -12,10 +14,13 @@ def main():
       if event.type == pygame.QUIT:
         return
 
-    screen.fill("black")
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    player.update(dt)
 
+    screen.fill("black")
+    player.draw(screen)
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
   main()
